cca67ab7af9bcf5690567fad3a7fe771fbaab58d
Burak Kurt <burakkurt2010@gmail.com>
2024-03-05 00:07:09 +0300

    Add project scripts

---
+++ _Scripts/Inventory/Extensions/UQueryBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UIElements;
+
+public static class UQueryBuilderExtensions {
+
+    public static IEnumerable<T> OrderBy<T, TKey>(this UQueryBuilder<T> query, Func<T, TKey> keySelector, Comparer<TKey> @default)
+        where T : VisualElement
+    {
+        return query.ToList().OrderBy(keySelector, @default);
+    }
+    
+    public static IEnumerable<T> SortByNumericValue<T>(this UQueryBuilder<T> query, Func<T, float> keySelector)
+        where T : VisualElement
+    {
+        return query.OrderBy(keySelector, Comparer<float>.Default);
+    }
+    
+    public static T FirstOrDefault<T>(this UQueryBuilder<T> query)
+        where T : VisualElement
+    {
+        return query.ToList().FirstOrDefault();
+    }
+    
+    public static int CountWhere<T>(this UQueryBuilder<T> query, Func<T, bool> predicate) 
+        where T : VisualElement 
+    {
+        return query.ToList().Count(predicate);
+    }
+}
---
+++ _Scripts/Inventory/InventoryView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Systems.Inventory {
+    public class InventoryView : StorageView {
+        [SerializeField] string panelName = "Inventory";
+
+        public override IEnumerator InitializeView(ViewModel viewModel) {
+            Slots = new Slot[viewModel.Capacity];
+            root = document.rootVisualElement;
+            root.Clear(); 
+
+            root.styleSheets.Add(styleSheet);
+ 
+            container = root.CreateChild("container");
+            
+            var inventory = container.CreateChild("inventory").WithManipulator(new PanelDragManipulator());
+            inventory.CreateChild("inventoryFrame");
+            inventory.CreateChild("inventoryHeader").Add(new Label(panelName));
+
+            var slotsContainer = inventory.CreateChild("slotsContainer");
+            for (int i = 0; i < viewModel.Capacity; i++) {
+                var slot = slotsContainer.CreateChild<Slot>("slot");
+                Slots[i] = slot;
+            }
+            
+            ghostIcon = container.CreateChild("ghostIcon");
+            ghostIcon.BringToFront();
+           
+            
+            yield return null; 
+        }
+    }
+}
